5d6d49093d6792d648b60918a87837db8f6c6181
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-11-30 19:33:29 -0300

    Adicionando fields de imagens para os fields.

--- contact/models.py
+++ contact/models.py
@@ -4,10 +4,11 @@ from django.utils import timezone
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# picture (imagem)  
+# category (foreign key), show (boolean),
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -15,6 +16,8 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} id:{self.id}'
