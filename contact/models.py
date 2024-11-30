709567e4f12d8cb25e1fb389542f513383e46fb6
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-11-30 00:09:57 -0300

    Registrando model na area administrativa do Django.

--- contact/models.py
+++ contact/models.py
@@ -15,3 +15,6 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} id:{self.id}'
