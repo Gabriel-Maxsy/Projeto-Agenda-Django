68b6d9709aafb9a055d1b5c2982670e43c8ab59d
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-11-30 19:51:16 -0300

    Adicionando campo de categoria (foreign key).

--- contact/admin.py
+++ contact/admin.py
@@ -4,7 +4,7 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', # tupla
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category', # tupla
     ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
@@ -12,3 +12,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    ordering =  'id',
--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,11 @@ from django.utils import timezone
 
 # Depois
 # owner (foreign key)
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -18,6 +23,11 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} id:{self.id}'
