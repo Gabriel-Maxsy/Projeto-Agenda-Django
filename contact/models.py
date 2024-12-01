3f271a55fbfb21c1e4b2704629a3535f0e8f563b
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-12-01 16:12:48 -0300

    Adicionando script para criar fake contacts.

--- contact/models.py
+++ contact/models.py
@@ -26,6 +26,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
