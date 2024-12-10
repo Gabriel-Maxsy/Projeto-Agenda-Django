36fcf332487110f4fee509a9c5825998de075553
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-12-10 14:30:18 -0300

    Criando o formulário e entendendo sobre o metodo "POST".

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -4,9 +4,18 @@ from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 
 def create(request):
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+
     context = {
 
     }
+
+    print(request.method)
 
     return render(
         request,
