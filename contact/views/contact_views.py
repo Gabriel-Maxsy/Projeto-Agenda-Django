08c3fb380a6a4e19fa4eaafe671a5bccc54e1d58
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-12-10 18:53:36 -0300

    Adicionando título ao create.html.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -16,7 +16,7 @@ def index(request):
 
     context = {
         'page_obj': page_obj,
-        'site_title': 'Contacts - '
+        'site_title': 'Create contact - '
     }
 
     return render(
@@ -66,7 +66,7 @@ def contact(request, contact_id):
         #  quanto passando direto na url do site como o arquivo urls mostra
     )
 
-    site_title = f'{single_contact.first_name} {single_contact.last_name} '
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
