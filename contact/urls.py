a6a4be8fb548e4162bc3d75650b5bd64df77ea70
Gabriel Maxsy <140831842+Gabriel-Maxsy@users.noreply.github.com>
2024-12-29 01:15:37 -0300

    Criando um formulário para atualizar dados do usuário.

--- contact/urls.py
+++ contact/urls.py
@@ -8,13 +8,14 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     
     # Contact (CRUD)
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
-    path('contact/create/', views.create, name='create'),
-    path('contact/<int:contact_id>/update', views.update, name='update'),
-    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'), # Read
+    path('contact/create/', views.create, name='create'), # Create
+    path('contact/<int:contact_id>/update', views.update, name='update'), # Update
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'), # Delete
 
     # User
     path('user/create/', views.register, name='register'),
     path('user/login/', views.login_view, name='login'),
     path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 ]
